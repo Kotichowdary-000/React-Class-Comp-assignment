8a9279156fb0c891cac512686c5e2554192a96ef
Kotichowdary <kotichowdary000@gmail.com>
2025-11-24 12:06:19 +0530

    ThemeDone

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 // src/App.js
 import React, { Fragment } from "react";
 import "./App.css";
-// import StatusMessage from "./Components/StatusMessage";
-// import ContactUs from "./Components/ContactUs";
-// import ToDoList from "./Components/ToDoList";
-// import ColorChange from "./Components/ColorChange";
-// import Routers from "./routes/Routers";
-// import Navbar from "./Components/Navbar";
-// import ThemeProvider from "./Components/ThemeProvider";
-// import "./Components/light.css";
-// import "./Components/dark.css";
+import StatusMessage from "./Components/StatusMessage";
+import ContactUs from "./Components/ContactUs";
+import ToDoList from "./Components/ToDoList";
+import ColorChange from "./Components/ColorChange";
+import Routers from "./routes/Routers";
+import Navbar from "./Components/Navbar";
+import ThemeProvider from "./Components/ThemeProvider";
+import "./Components/light.css";
+import "./Components/dark.css";
 import CountWords from "./Components/CountWords";
 
 
 
 function App() {
   return (
-      <Fragment>
-{/* <ThemeProvider>
-   <Navbar />
+    <Fragment>
+      <ThemeProvider>
+        <Navbar />
         <Routers />
-  </ThemeProvider> */}
-  <CountWords />
+      </ThemeProvider>
+      {/* <CountWords /> */}
 
-       
-      </Fragment>
+
+    </Fragment>
   );
 }
 
